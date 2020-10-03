2c302e14a3e39293afd4df66daee316d7b682967
ArtemSuslov99 <artem1999370@gmail.com>
2020-10-03 14:19:40 +0300

    Class work

--- 02/ClassWork/2nd lesson/2nd lesson/Program.cs
+++ 02/ClassWork/2nd lesson/2nd lesson/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Dynamic;
+using System.Threading;
 
 namespace _2nd_lesson
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			//char letter = 'A';
+			//Console.WriteLine(letter);
 
+			//string name = "Bob";
+			//Console.WriteLine(name);
+
+			//byte age = 36;
+			//Console.WriteLine(age);
+
+			Console.Write("Введите ваш вораст: ");
+			string inutAge = Console.ReadLine(); 
+			int age = int.Parse(inutAge);
+			Console.WriteLine($"Ваш вораст чере 10 лет: { age + 10}");
 		}
 	}
 }
