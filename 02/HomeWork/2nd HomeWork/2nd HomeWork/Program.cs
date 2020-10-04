00e5758426c4edfc8c06a4e68139c1c5f5b05533
ArtemSuslov99 <artem1999370@gmail.com>
2020-10-04 13:03:40 +0300

    Home work oh the 2nd lesson added

---
+++ 02/HomeWork/2nd HomeWork/2nd HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2nd_HomeWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.Write("Введите первое число: ");
+			string firstInputNumber = Console.ReadLine();
+			int firstNumber = int.Parse(firstInputNumber);
+			Console.Write("Введите второе число: ");
+			string secondInputNumber = Console.ReadLine();
+			int secondNumber = int.Parse(secondInputNumber);
+			Console.WriteLine($"Сумма чисел равна: {firstNumber + secondNumber}");
+			Console.WriteLine($"Разница чисел равна: {firstNumber - secondNumber}");
+			Console.WriteLine($"Произведение чисел равна: {firstNumber * secondNumber}");
+
+		}
+	}
+}
