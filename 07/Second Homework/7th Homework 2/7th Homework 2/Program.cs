f971c2cc7e592025c0965875ade7c4ce9642a8f0
ArtemSuslov99 <artem1999370@gmail.com>
2020-10-25 10:50:34 +0300

    7th Homework2 added

---
+++ 07/Second Homework/7th Homework 2/7th Homework 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _7th_Homework_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string text;
+			int num;
+			bool isNum;
+			Console.Write("Введите текст: ");
+
+			do
+			{
+				text = Console.ReadLine();
+				isNum = int.TryParse(text, out num);
+				if (isNum == false)
+				{
+					try
+					{
+						StringBuilder sb = new StringBuilder(text.Length);
+						for (int i = 0; i < sb.Capacity; i++)
+							sb.Insert(0, text[i]);
+						Console.WriteLine(sb.ToString().ToLower());
+					}
+					catch
+					{
+						Console.Write("Вы ввели пустую строку! Попробуйте ещё раз: ");
+						continue;
+					}
+					
+				}
+				else
+				{
+					Console.Write("Вы ввели не строковое значение! Введите строковое значение: ");
+					continue;
+				}
+			} while (string.IsNullOrWhiteSpace(text) || isNum == true);
+		}
+	}
+
+}
