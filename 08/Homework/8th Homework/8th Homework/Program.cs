ae1dd426d007271216da4752014875b44e6ea7dc
ArtemSuslov99 <artem1999370@gmail.com>
2020-10-29 23:52:35 +0300

    8th Homework added

---
+++ 08/Homework/8th Homework/8th Homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8th_Homework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите строку из скобок такого вида: ( ), [ ]: ");
+			string input = Console.ReadLine();
+			char[] buffArray = input.ToCharArray();
+			bool output = true;
+
+			Dictionary<char, char> bracketDictionary = new Dictionary<char, char>();
+			bracketDictionary.Add(')', '(');
+			bracketDictionary.Add(']', '[');
+			Stack<char> stackOfBrackets = new Stack<char>();
+			foreach (char symbol in buffArray)
+			{
+				if (bracketDictionary.ContainsValue(symbol))
+				{
+					stackOfBrackets.Push(symbol);
+				}
+				else if (bracketDictionary.ContainsKey(symbol))
+				{
+					if (stackOfBrackets.Count > 0 && stackOfBrackets.Peek() == bracketDictionary[symbol])
+						stackOfBrackets.Pop();
+					else
+						output = false;
+				}
+			}
+			if (stackOfBrackets.Count > 0)
+				output = false;
+			Console.WriteLine(output);
+			Console.ReadKey();
+		}
+	}
+}
