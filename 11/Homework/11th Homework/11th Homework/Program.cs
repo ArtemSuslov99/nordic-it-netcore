f2f14bc891764bca5bec4e6c8f6f48e1ffbe4ed3
ArtemSuslov99 <artem1999370@gmail.com>
2020-11-08 11:51:15 +0300

    11th Homework added

---
+++ 11/Homework/11th Homework/11th Homework/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _11th_Homework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTimeOffset reminderDateTime;
+			string reminderMessage;
+			Console.WriteLine("Введите количество будильников: ");
+			int amountOfReminders = int.Parse(Console.ReadLine());
+			ReminderItem[] reminders = new ReminderItem[2];
+			for (int i = 0; i < 2; i++)
+			{
+				Console.Write($"Введите время будильника №{i + 1}:");
+				try
+				{
+					reminderDateTime = DateTimeOffset.Parse(Console.ReadLine());
+					Console.Write($"Введите сообщение будильника №{i + 1}:");
+					reminderMessage = Console.ReadLine();
+					reminders[i] = new ReminderItem(reminderMessage, reminderDateTime);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine($"Ошибка {e.Message}, попробуйте еще раз!");
+					i--;
+					continue;
+				}
+			}
+			for (int i = 0; i < 2; i++)
+			{
+				reminders[i].WriteProperties();
+			}
+		}
+	}
+}
---
+++ 11/Homework/11th Homework/11th Homework/ReminderItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace _11th_Homework
+{
+	class ReminderItem
+	{
+		public DateTimeOffset AlarmDate { get; set; }
+		public string AlarmMessage { get; set; }
+		public TimeSpan TimeToAlarm
+		{
+			get
+			{
+				return AlarmDate - DateTimeOffset.Now;
+			}
+		}
+		public bool IsOutdated
+		{
+			get
+			{
+				bool IsOutdated;
+				if (TimeToAlarm > TimeSpan.Zero)
+				{
+					IsOutdated = true;
+				}
+				else
+					IsOutdated = false;
+				return IsOutdated;
+			}
+		}
+		public ReminderItem() { }
+
+		public ReminderItem(string alarmMessage, DateTimeOffset alarmDate)
+		{
+			AlarmMessage = alarmMessage;
+			AlarmDate = alarmDate;
+		}
+
+		public void WriteProperties()
+		{
+			string outDated = IsOutdated
+				? "будильник не просрочен"
+				: "будильник просрочен";
+			Console.WriteLine($"Дата и время будильника: {AlarmDate}, " +
+				$" текст будильника: {AlarmMessage}, " +
+				$"\nвремя до срабатывания будильника: {TimeToAlarm}, {outDated}");
+		}
+
+	}
+}
