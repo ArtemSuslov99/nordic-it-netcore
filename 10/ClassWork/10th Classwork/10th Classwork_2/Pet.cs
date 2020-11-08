ec3706f94add40a7cb9548362002d80e5bcd7bbd
ArtemSuslov99 <artem1999370@gmail.com>
2020-11-08 11:50:13 +0300

    10th Classwork added

---
+++ 10/ClassWork/10th Classwork/10th Classwork_2/Pet.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10th_Classwork_2
+{
+
+	class Pet
+	{
+		public PetKind Kind;
+		private string _birthPlace;
+		private char _sex;
+		private byte _age;
+		public string Name { get; set; }
+		public int Age { get; set;}
+		public DateTimeOffset DateOfBirth;
+
+
+		public byte GetAge()
+		{
+			TimeSpan liveTime = DateTimeOffset.Now.Subtract(DateOfBirth);
+			return (byte)Math.Floor(liveTime.TotalDays / 365.242);
+
+		}
+
+		public string ShortDescription
+		{
+			get
+			{
+				return $"Имя:{Name}, возраст {GetAge()}";
+			}
+		}
+
+		public Pet(string name, PetKind kind, char sex, DateTimeOffset dateofbirth, string birthplace)
+		{
+			Name = name;
+			Kind = kind;
+			Sex = sex;
+			DateOfBirth = dateofbirth;
+			_birthPlace = birthplace;
+		}
+
+		public string GetShortDescription(bool isShort = false)
+		{
+			return isShort
+				? ShortDescription
+				: PropertiesString;
+		}
+
+
+
+		public void UpdateProperties(string name)
+		{
+			Name = name;
+		}
+
+		public void UpdateProperties(string name, char sex)
+		{
+			Name = name;
+			Sex = sex;
+
+		}
+
+		public string PropertiesString
+
+		{
+			get
+			{
+				return $"{Name} is a {Kind} ({Sex}) of {GetAge()} years old birth place: {_birthPlace}.";
+			}
+		}
+		public string GetPropertiesString()
+		{
+			return $"{Name} is a {Kind} ({Sex}) of {GetAge()} years old.";
+		}
+
+		public void SetBirthPlace(string birthPlace)
+		{
+			if (birthPlace == null)
+			{
+				throw new ArgumentNullException(nameof(birthPlace));
+			}
+
+			if (_birthPlace != null)
+			{
+				throw new Exception(message: "Birth place cannot be overriden.");
+			}
+			_birthPlace = birthPlace;
+		}
+
+
+		public char Sex
+		{
+			get { return _sex; }
+			set
+			{
+				if (value != 'M' && value != 'm' && value != 'F' && value != 'f')
+					throw new Exception(message: "Sex should be one of two options: M/F");
+
+				_sex = char.ToUpper(value);
+			}
+		}
+
+
+	}
+
+}
--- 10/ClassWork/10th Classwork/10th Classwork_2/Program.cs
+++ 10/ClassWork/10th Classwork/10th Classwork_2/Program.cs
@@ -9,63 +9,6 @@ namespace _10th_Classwork_2
 		Dog
 	}
 
-	class Pet
-	{
-		public PetKind Kind;
-		private string _birthPlace;
-		private char _sex;
-		private byte _age;
-		public string Name { get; set; }
-		public string PropertiesString
-		{
-			get
-			{
-				return $"{Name} is a {Kind} ({Sex}) of {Age} years old birth place: {_birthPlace}.";
-			}
-		}
-		public string GetPropertiesString()
-		{
-			return $"{Name} is a {Kind} ({Sex}) of {Age} years old.";
-		}
-
-		public void SetBirthPlace(string birthPlace)
-		{
-			if (birthPlace == null)
-			{
-				throw new ArgumentNullException(nameof(birthPlace));
-			}
-
-			if (_birthPlace != null)
-			{
-				throw new Exception(message: "Birth place cannot be overriden.");
-			}
-			_birthPlace = birthPlace;
-		}
-
-
-		public char Sex
-		{
-			get { return _sex; }
-			set
-			{
-				if (value != 'M' && value != 'm' && value != 'F' && value != 'f')
-					throw new Exception(message: "Sex should be one of two options: M/F");
-
-				_sex = char.ToUpper(value);
-			}
-		}
-		public byte Age
-		{
-			get { return _age; }
-			set
-			{
-				if (value > 150)
-					throw new Exception(message: "Age can not be more than 150");
-				_age = value;
-			}
-		}
-
-	}
 	class Program
 	{
 		static void Main(string[] args)
@@ -73,22 +16,20 @@ namespace _10th_Classwork_2
 			Pet pet1 = new Pet();
 			pet1.Kind = PetKind.Cat;
 			pet1.Name = "Barsik";
-			pet1.Age = 8;
+			pet1.DateOfBirth = DateTimeOffset.Parse("2012-11-07 20:20:15.5488");
 			pet1.Sex = 'M';
 			pet1.SetBirthPlace("Moscow");
-			Console.WriteLine(pet1.PropertiesString);
+			pet1.UpdateProperties(name: "Tom");
 
 			Pet pet2 = new Pet();
 			pet2.Kind = PetKind.Dog;
 			pet2.Name = "Elly";
-			pet2.Age = 2;
+			pet2.DateOfBirth = DateTimeOffset.Parse("2019-11-06 20:20:15.5488");
 			pet2.Sex = 'F';
 			pet2.SetBirthPlace("Deutschland");
-			Console.WriteLine(pet2.PropertiesString);
+			pet2.UpdateProperties(name: "Tom", sex: 'M');
 
-
+			Pet pet3 = new Pet(name:"Tom", PetKind.Dog, sex: 'M');
 		}
-
-
 	}
 }
