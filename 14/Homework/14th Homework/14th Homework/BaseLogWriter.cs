6c4616cad2fcfa795f4d4abfb62dd4756a288e0b
ArtemSuslov99 <artem1999370@gmail.com>
2020-11-24 12:29:22 +0300

    14th Homework added

---
+++ 14/Homework/14th Homework/14th Homework/BaseLogWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14th_Homework
+{
+    public abstract class BaseLogWriter : ILogWriter
+    {
+        public void LogInfo(string message)
+        {
+            WriteMessage(message, MessageType.Info);
+        }
+
+        public void LogWarning(string message)
+        {
+            WriteMessage(message, MessageType.Warning);
+        }
+
+        public void LogError(string message)
+        {
+            WriteMessage(message, MessageType.Error);
+        }
+
+        public abstract void WriteMessage(string message, MessageType type);
+
+        protected virtual string FormatMessage(string message, MessageType type)
+        {
+            return $"{DateTimeOffset.Now:yyyy-MM-ddTHH:MM:ss+0000} {type} {message}";
+        }
+    }
+}
---
+++ 14/Homework/14th Homework/14th Homework/MultipleLogWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _14th_Homework
+{
+    class MultipleLogWriter : BaseLogWriter
+    {
+        private IEnumerable<ILogWriter> _logWriters;
+
+        public MultipleLogWriter(params ILogWriter[] writers)
+        {
+            _logWriters = writers;
+        }
+
+        public override void WriteMessage(string message, MessageType type)
+        {
+            foreach (var item in _logWriters)
+            {
+                switch (type)
+                {
+                    case MessageType.Error:
+                        item.LogError(message);
+                        break;
+                    case MessageType.Info:
+                        item.LogInfo(message);
+                        break;
+                    case MessageType.Warning:
+                        item.LogWarning(message);
+                        break;
+                }
+            }
+        }
+    }
+}
